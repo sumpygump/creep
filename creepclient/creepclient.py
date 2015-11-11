906b0ab094834fdfd1be5b880ad8191063eb2a38
Jansen Price <sumpygump@gmail.com>
2015-11-11 00:17:04 -0600

    0.1.2

--- creepclient/creepclient.py
+++ creepclient/creepclient.py
@@ -19,7 +19,7 @@ class CreepClient(Client, cmd.Cmd):
     """Creep Mod Package Manager Client"""
 
     # Version of this client
-    VERSION = '0.1.1'
+    VERSION = '0.1.2'
 
     # Absolute path where this client is installed
     installdir = ''
